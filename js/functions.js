fbdcb6e1689e20822da8f2b8f408c3a8082a30c1
Andres Quintero <libertown@gmail.com>
2014-09-09 17:15:42 -0400

    again

--- js/functions.js
+++ js/functions.js
@@ -39,14 +39,15 @@ $(document).ready(function() {
 			}
 		);
 
+        $("#cerrar").on('click', function () { 
+            $(".profile").animate({right:'-350px'},300); 
+        });
+
 		$(function(){
 			$(".notificaciones").hide();
 			$("#btn2").click(function(){$(".notificaciones").slideToggle(300);})	
 		});
-		
-});
 
-$(document).ready(function() {
 		var  block_in = false;
         $("#btn3").toggle(
 			function () { 
@@ -58,9 +59,134 @@ $(document).ready(function() {
 			block_in = false;
 			}
 		);	
-				
+		
 });
 
+$(document).click(function (e)
+{
+    var container = $(".question");
+
+    if (!container.is(e.target) 
+        && container.has(e.target).length === 0
+        && container.is(":visible"))
+    {
+        container.animate({left:'-300px'},600);
+    }
+
+     var container2 = $(".profile");
+
+    if (!container2.is(e.target) 
+        && container2.has(e.target).length === 0
+        && container2.is(":visible"))
+    {
+        container2.animate({right:'-350px'},600); 
+    }
+});
+
+
+$(document).ready(function(){
+  $("#act_btn_1").click(function(){
+        var numero = 33;
+        $("#video").css("display","none");
+        $("#video").animate({left:'-600px'});
+        $("#evaluacion").css("display","block");
+        $("#evaluacion").animate({left:'8px'});
+        $("#progressBar").html(numero);
+        $("#status").html("Phase 2 of 3");
+  });
+  $("#act_btn_2").click(function(){
+        var numero = 33;
+        $("#evaluacion").css("display","none");
+        $("#evaluacion").animate({left:'-600px'});
+        $("#video").css("display","block");
+        $("#video").animate({left:'8px'});
+        $("#progressBar").html(numero);
+        $("#status").html("Phase 3 of 3");
+  });
+  $("#act_btn_3").click(function(){
+        var numero = 33;
+        $("#video").css("display","none");
+        $("#evaluacion").css("display","none");
+        $(".activities").css("height","0px");
+        $("#video").animate({left:'-600px'});
+        $(".discusion").css("display","block");
+        $(".discusion").animate({left:'8px'});
+        $("#progressBar").html(numero);
+        $("#status").html("Phase 3 of 3");
+  });
+
+});
+
+
+$(document).ready(function(){
+                $("#uno").click(function(){
+                $("#slide1").css("display","none");
+                $(".one").css("background-color","#5fcf80");
+                $(".two").css("background-color","#5fcf80");
+                $(".three").css("background-color","#575756");
+                $(".four").css("background-color","#575756");
+                $("#slide2").animate({right:'0px'});
+                $("#slide_title").html("Modulos de curso");
+                $("#slide_txt").html("Visualiza e interactua con los modulo del curso que seleccionaste");
+                $("#uno").hide();
+                $("#dos").show();
+                });
+
+                $("#dos").click(function(){
+                $("#slide2").css("display","none");
+                $(".three").css("background-color","#5fcf80");
+                $("#slide3").animate({right:'0px'});
+                $("#slide_title").html("Actividades de Modulo");
+                $("#slide_txt").html("Accede a las actividades y controla tu progreso del módulo.");
+                $("#dos").hide();
+                $("#tres").show();
+                });
+
+                $("#tres").click(function(){
+                $("#slide3").css("display","none");
+                $(".four").css("background-color","#5fcf80");
+                $("#slide4").animate({right:'0px'});
+                $("#slide_title").html("Listo para empezar a aprender");
+                $("#slide_txt").html("Puedes preguntar al facilitador levantando la mano");
+                $("#tres").hide();
+                $("#cuatro").show();
+                });
+
+         });
+
+    $(function() {
+    var width = 245;
+    var animationSpeed = 1000;
+    var pause = 4000;
+    var currentSlide = 1;
+    var $slider = $('.atr_slider_wrap');
+    var $slideContainer = $('.atr_mobile_blocks', $slider);
+    var $slides = $('.atr_mobile_block', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
 
 
 
