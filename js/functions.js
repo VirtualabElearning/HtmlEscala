80c52362844eaa01188725dc7377eb5b99e093c0
Andres Quintero <libertown@gmail.com>
2014-10-24 09:00:16 -0400

    Cambios a 24 de Octubre
    Animacion Puntaje, Puntaje se anima cuando el popup de caja sorpresa
    sale. hacer click en la actividad 4 para ver.

--- js/functions.js
+++ js/functions.js
@@ -234,7 +234,35 @@ $(document).ready(function(){
         },pause);
 
     });
-
+    $(document).ready(function() {(function($) {
+            $.fn.countTo = function(options) {
+                options = $.extend({}, $.fn.countTo.defaults, options || {});
+                var loops = Math.ceil(options.speed / options.refreshInterval),
+                    increment = (options.to - options.from) / loops;return $(this).each(function() {
+                    var _this = this,
+                        loopCount = 0,
+                        value = options.from,
+                        interval = setInterval(updateTimer, options.refreshInterval);
+                        function updateTimer() {
+                        value += increment;
+                        loopCount++;
+                        $(_this).html(value.toFixed(options.decimals));
+                        if (typeof(options.onUpdate) == 'function') {
+                            options.onUpdate.call(_this, value);}
+                            if (loopCount >= loops) {
+                            clearInterval(interval);
+                            value = options.to;
+                            if (typeof(options.onComplete) == 'function') {
+                                options.onComplete.call(_this, value);
+                            }
+                        }
+                    }
+                });
+            };$.fn.countTo.defaults = {
+                from: 0,to: 100,speed: 1000,refreshInterval: 100,decimals: 0,onUpdate: null,onComplete: null,  // callback method for when the element finishes updating
+            };
+        })(jQuery); 
+ });
    
     $(document).ready(function(){
         var activity_btn = $("#act_btn_1");
@@ -320,6 +348,7 @@ $(document).ready(function(){
 
     });
 
+   
 
     
     
